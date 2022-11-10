12680660923bd74054cae40d313225bc6c486b02
SalvaVE <valladares.salvador12@gmail.com>
2022-11-10 06:52:28 -0600

    login y registrar por revisar el funcionamiento

--- app/src/main/java/com/fjar/trasnportfast/ui/Login.java
+++ app/src/main/java/com/fjar/trasnportfast/ui/Login.java
@@ -2,15 +2,76 @@ package com.fjar.trasnportfast.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
+import com.fjar.trasnportfast.MainActivity;
 import com.fjar.trasnportfast.R;
 
+
 public class Login extends AppCompatActivity {
+    private Button btnRegistrar, btnIniciar;
+    private EditText Nempresa, contrasena;
+    private Switch holdSession;
+    private dto_empresa empresa = new dto_empresa();
+    private EmpresaCRUD CRUD = new EmpresaCRUD();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
+        setContentView(R.layout.activity_login);empresa = new dto_empresa();
+
+                    Intent nueva = new Intent(Login.this, MainActivity.class);
+                    startActivity(nueva);
+        //Instanciamos nuestros componentes
+        Nempresa = (EditText) findViewById(R.id.etuser);
+        contrasena = (EditText) findViewById(R.id.etclave);
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrarEmp);
+        btnIniciar = (Button) findViewById(R.id.btn_log);
+
+
+
+
+        //Funcion de los botones
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent cambiarVista = new Intent(Login.this, Registrar.class);
+                startActivity(cambiarVista);
+            }
+        });
+        btnIniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usu = Nempresa.getText().toString();
+                String contra = contrasena.getText().toString();
+
+                if(usu.length() == 0){
+                    Nempresa.setError("Campo obligatorio");
+                }else if(contra.length() == 0){
+                    contrasena.setError("Campo obligatorio");
+                    }else{
+                        empresa.setNombre(usu);
+                    }
+                    empresa.setClave(contra);
+
+                    CRUD.IniciarSesionemp(Login.this, empresa, holdSession);
+
+                        Intent nueva = new Intent(Login.this, MainActivity.class);
+                        startActivity(nueva);
+                    }
+
+
+        });
+                }
 }
+
+
--- app/src/main/java/com/fjar/trasnportfast/ui/Registrar.java
+++ app/src/main/java/com/fjar/trasnportfast/ui/Registrar.java
@@ -2,15 +2,66 @@ package com.fjar.trasnportfast.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.fjar.trasnportfast.R;
 
 public class Registrar extends AppCompatActivity {
+    private EditText nombre, telefono, correo, direccion, CP, clave;
+    private Button btnRegistrar;
+    private EmpresaCRUD CRUD = new EmpresaCRUD();
+    private dto_empresa dtoEmpresa = new dto_empresa();
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar);
+        nombre = (EditText) findViewById(R.id.edt_NomEmp);
+        telefono = (EditText) findViewById(R.id.edt_NumEmp);
+        correo = (EditText) findViewById(R.id.edt_correoEmp);
+        direccion = (EditText) findViewById(R.id.edt_direccionEmp);
+        CP = (EditText) findViewById(R.id.edt_codigoP);
+        clave = (EditText) findViewById(R.id.edt_claveEmp);
+        btnRegistrar = (Button) findViewById(R.id.btn_registrar);
+
+
+
+
+
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Pasar datos de los edit text a variables String
+                dtoEmpresa.setNombre(nombre.getText().toString());
+                dtoEmpresa.setTelefono(telefono.getText().toString());
+                dtoEmpresa.setCorreo(correo.getText().toString());
+                dtoEmpresa.setDireccion(direccion.getText().toString());
+                dtoEmpresa.setCodigo_postal(CP.getText().length());
+                dtoEmpresa.setClave(clave.getText().toString());
+                if(dtoEmpresa.getNombre().length() == 0){
+                    nombre.setError("Campo obligatorio.");
+                }else if(dtoEmpresa.getTelefono().length() == 0){
+                    telefono.setError("Campo obligatorio.");
+                }else if(dtoEmpresa.getCorreo().length() == 0){
+                    correo.setError("Campo obligatorio.");
+                }else if(dtoEmpresa.getDireccion().length() == 0){
+                    direccion.setError("Campo obligatorio.");
+                }else if(dtoEmpresa.getCodigo_postal() == 0){
+                    CP.setError("Campo obligatorio.");
+                }else if(dtoEmpresa.getClave().length() == 0){
+                    clave.setError("Campo obligatorio.");
+                }else{
+                    CRUD.registrarUsuario(Registrar.this, dtoEmpresa);
+                }
+            }
+        });
     }
 }
